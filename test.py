750d8951e0e2bf764aafc5fb7038f4820a77e7cd
artem <temathebst@gmail.com>
2024-05-26 00:48:29 +0600

    [+] done sarimax (bad predictions)

--- test.py
+++ test.py
@@ -1,124 +1,61 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 
-# Функция для загрузки данных из CSV
-# def load_data(file_path):
-#     data = pd.read_csv(file_path, index_col='DATE', parse_dates=True)
-#     return data
+df = pd.read_csv("1_model.csv")
 
-# # Функция для нормализации данных
-# def normalize_data(data):
-#     scaler = MinMaxScaler()
-#     data_normalized = scaler.fit_transform(data)
-#     return data_normalized, scaler
+date_columns = [col for col in df.columns if 'date_tag' in col]
+value_columns = [col for col in df.columns if 'value_tag' in col]
 
-# # Функция для подготовки данных для LSTM
-# def create_dataset(data, time_steps=1):
-#     X, y = [], []
-#     for i in range(len(data) - time_steps):
-#         X.append(data[i:(i + time_steps)])
-#         y.append(data[i + time_steps])
-#     return np.array(X), np.array(y)
-
-# # Функция для построения графика
-# def plot_data(data, predictions=None, title='Time Series Data'):
-#     lastData = data.values[19000:19600:1]
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(lastData, label='Actual Data')
-#     if predictions is not None:
-#         plt.plot(range(len(lastData), len(lastData) + len(predictions)), predictions, label='Predictions')
-#     plt.title(title)
-#     plt.xlabel('Time')
-#     plt.ylabel('Values')
-#     plt.legend()
-#     plt.show()
-
-# # Функция для создания и обучения модели LSTM
-# def train_lstm_model(X_train, y_train, epochs=20, batch_size=32):
-#     model = Sequential()
-#     model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-#     model.add(Dropout(0.2))
-#     model.add(LSTM(units=50))
-#     model.add(Dropout(0.2))
-#     model.add(Dense(units=1))
-
-#     model.compile(optimizer='adam', loss='mean_squared_error')
-#     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1)
-#     return model
-
-# # Основная функция для загрузки данных, нормализации, обучения модели и прогнозирования
-# def main(file_path, time_steps=50, epochs=10, batch_size=32, future_steps=1000):
-#     data = load_data(file_path)
-#     print("Data loaded successfully.")
-
-#     # Нормализация данных
-#     data_normalized, scaler = normalize_data(data)
-#     print("Data normalized successfully.")
-
-#     # Подготовка данных для LSTM
-#     X, y = create_dataset(data_normalized, time_steps)
-#     X_train, X_test = X[:-future_steps], X[-future_steps:]
-#     y_train, y_test = y[:-future_steps], y[-future_steps:]
-#     print("Data prepared for LSTM successfully.")
-
-#     # Обучение модели LSTM
-#     model = train_lstm_model(X_train, y_train, epochs, batch_size)
-#     print("LSTM model trained successfully.")
-
-#     # Прогнозирование будущих значений
-#     predictions = []
-#     current_batch = X_test[0].reshape((1, time_steps, X_test.shape[2]))
-#     for i in range(future_steps):
-#         current_pred = model.predict(current_batch)[0]
-#         predictions.append(current_pred)
-#         current_batch = np.append(current_batch[:, 1:, :], [[current_pred]], axis=1)
-#     predictions = scaler.inverse_transform(predictions)
-#     print("Future data predicted successfully.")
-#     # Построение графика
-#     plot_data(data, predictions, title='Time Series Data and Predictions')
-#     print("Graph plotted successfully.")
-
-# # Вызов основной функции
-# file_path = 'test.csv'  # Укажите путь к вашему CSV файлу
-# main(file_path)
-
-def load_data(file_path):
-    data = pd.read_csv(file_path, parse_dates=True)
-    return data
-
-def groupData(df):
-    result = []
-    for i in range(1, len(df.columns), 2):
-        if (i + 1 < len(df.columns)):
-            result.append(pd.DataFrame({'time': pd.to_datetime(df.iloc[:, i], format='%Y-%m-%dT%H:%M:%S.%f', errors='coerce'), 'value': df.iloc[:, i + 1]}))
-    return result
+for col in date_columns:
+    df[col] = pd.to_datetime(df[col])
 
 
+df.dropna(subset=date_columns + value_columns, inplace=True)
 
-def printGraphics(array):
-    num_plots = len(array)
-    plt.figure(figsize=(10, num_plots * 3))  # Adjust the figure size as needed
+data = pd.DataFrame()
 
-    for idx, el in enumerate(array):
-        el = el[el['value'].notna()]  # Filter out rows where 'value' is NaN
-        el['time'] = pd.to_datetime(el['time'])  # Ensure 'time' is in datetime format
+for i, col in enumerate(date_columns):
+    data[col] = df[col]
+    data[f'Value_{i}'] = df[value_columns[i]]
 
-        ax = plt.subplot(num_plots, 1, idx + 1)  # Create a subplot for each element
-        ax.plot(el['time'], el['value'])
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Value')
-        ax.set_title(f'Plot {idx + 1}')
+data.set_index(date_columns[0], inplace=True)
 
-    plt.tight_layout()
-    plt.show()
+data.interpolate(method='ffill', inplace=True)
 
+num_plots = len(date_columns)
 
-file_path = '1_model.csv'
-df = load_data(file_path)
-groupedData = groupData(df)
-printGraphics(groupedData)
+num_cols = 2
+num_rows = (num_plots + num_cols - 1) // num_cols
+
+fig, axs = plt.subplots(num_rows, num_cols, figsize=(14, 7*num_rows))
+
+for i in range(num_plots):
+    y = data[f'Value_{i}']
+
+    train = y[:'2022-03-15']
+    test = y['2022-03-15':]
+
+    model = SARIMAX(train, order=(5, 1, 0), seasonal_order=(1, 1, 1, 12))
+    model_fit = model.fit()
+
+    forecast_steps = len(test)
+    y_pred = model_fit.forecast(steps=forecast_steps)
+
+    row = i // num_cols
+    col = i % num_cols
+
+    axs[row, col].plot(train.index, train, label='Train')
+    axs[row, col].plot(test.index, test, label='Test')
+    axs[row, col].plot(test.index, y_pred, color='darkgreen', label='Predictions')
+    axs[row, col].set_title(f'Value_{i}')
+    axs[row, col].legend()
+
+for i in range(num_plots, num_rows * num_cols):
+    row = i // num_cols
+    col = i % num_cols
+    axs[row, col].remove()
+
+plt.tight_layout()
+plt.show()
